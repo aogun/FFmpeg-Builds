a0c25251c07aa72a9147d8c8eeba5e6028948998
aogun <angus_ag@msn.com>
2021-07-21 15:51:22 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -63,7 +63,7 @@ cat <<EOF >"$BUILD_SCRIPT"
     git checkout $GIT_BRANCH
 
     ./configure --prefix=/ffbuild/prefix --pkg-config-flags="--static" \$FFBUILD_TARGET_FLAGS $FF_CONFIGURE \
-        --extra-cflags='$FF_CFLAGS' --extra-cxxflags='$FF_CXXFLAGS' \
+        --extra-cflags='$FF_CFLAGS' --disable-stripping --extra-cxxflags='$FF_CXXFLAGS' \
         --extra-ldflags='$FF_LDFLAGS' --extra-ldexeflags='$FF_LDEXEFLAGS' --extra-libs='$FF_LIBS' \
         --extra-version="\$(date +%Y%m%d)"
     make -j\$(nproc) V=1
